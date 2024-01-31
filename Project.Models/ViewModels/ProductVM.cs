b9dcb7396f2cca4b53ba0f6e39f87ee1052d8c0f
Momen safi <Safi.moumen90@gmail.com>
2024-01-31 16:28:26 +0300

    continue

---
+++ Project.Models/ViewModels/ProductVM.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookWeb.Models.ViewModels
+{
+    public class ProductVM
+    {
+        public Product product { get; set; }
+        public IEnumerable<SelectListItem> Category { get; set; }
+    }
+}
